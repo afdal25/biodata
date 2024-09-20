ef1a8479ecc8c4cbdcbe58bfae80f0dba960f198
afdal <afdalfa013@gmail.com>
2024-09-20 17:53:35 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+alert('Hallo, selamat datang!');
+
+console.log('Nama : Mochamad Afdal');
+console.log('NIm : 20230040323');
+console.log('Jurusan : Teknik Informatika');
+console.log('Tempat/Tgl Lahir : Sukabumi, 25 Mei 2005');
+console.log('Alamat : Sukaraja, Sukabumi');
